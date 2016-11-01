da61404a114546e6f39523b00560905d7a776ce4
nogna <albin.sundqvist93@gmail.com>
2016-11-01 05:38:08 -0700

    dl file avg

---
+++ labb1/tftp_python/avg_time_dl_measurements.py
@@ -0,0 +1,26 @@
+import time
+import sys
+import subprocess
+import os
+
+start_time = time.time()
+file_to_dl = sys.argv[1]
+
+
+
+if len(sys.argv) == 3:
+	number_of_runs = int(sys.argv[2])
+
+current_run = 0
+while current_run < number_of_runs:
+	try:
+		os.remove(file_to_dl)
+	except OSError:
+		pass
+
+	downloaded_file = subprocess.call('python tftp.py -g ' + str(file_to_dl) + " rabbit.it.uu.se", shell=True)
+	current_run = current_run+1
+
+total_time = ((time.time() - start_time)/current_run)
+
+print "Avg time:"+ str(total_time) + " seconds --------  Number of samples:" + str(current_run) 
